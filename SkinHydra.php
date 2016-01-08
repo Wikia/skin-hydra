1835c5c7e7684feb8c4201e05d041a4cc89bbea1
Alexia E. Smith <washuu@gmail.com>
2016-01-08 14:50:00 -0600

    Missing file.

---
+++ SkinHydra.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * SkinTemplate class for Hydra skin
+ * @ingroup Skins
+ */
+class SkinHydra extends SkinVector {
+	public $skinname = 'hydra';
+	public $stylename = 'Hydra';
+	public $template = 'HydraTemplate';
+}
