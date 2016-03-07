433385eb7f9e5c4f29a42b5d5048521259819945
Cameron Chunn <cchunn@curse.com>
2016-03-07 14:39:51 -0600

    jQuery style loader, and lots of silly checks

--- js/footer.js
+++ js/footer.js
@@ -2,21 +2,24 @@
  *  Magical Footer Mover.
  *  Make sure its always below the nav panel.
  */
-(function(){
+
+$(function(){
     $footer = $('footer');
     $nav = $('#mw-panel');
-    new ResizeSensor($nav[0],function(){ handleResizeEvents(); });
-    $( window ).resize(function(){ handleResizeEvents(); });
+    if (typeof $footer !== 'undefined'
+        && typeof $nav !== 'undefined'
+        && typeof $nav[0] !== 'undefined'
+    ) {
+        new ResizeSensor($nav[0],function(){ handleResizeEvents(); });
+        $( window ).resize(function(){ handleResizeEvents(); });
+    }
     handleResizeEvents = function(){
-
         var navPosition = $nav.position();
         var footerPosition = $footer.position();
-
         if (navPosition
             && footerPosition
             && typeof navPosition.top !== 'undefined'
             && typeof footerPosition.top !== 'undefined') {
-
                 var navHeight = $nav.height() + navPosition.top;
                 var footerTop = footerPosition.top;
                 if (footerTop < navHeight) {
@@ -26,6 +29,6 @@
                     $footer.css('margin-top','0px');
                 }
         }
+    }
 
-    };
-}());
+});
