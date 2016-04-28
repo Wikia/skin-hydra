1a2600968542e2d398040edc9c5b97af5d0a5204
Alexia E. Smith <washuu@gmail.com>
2016-04-28 16:18:03 -0500

    CSS

--- js/mobileads.js
+++ js/mobileads.js
@@ -3,7 +3,7 @@
 		if (window.mobileatfmrec && window.mobileatfmrec.length) {
 			var headings = $("#bodyContent").find("h1, h2, h3, h4, h5, h6");
 			if ($(headings).length > 2) {
-				$(headings[2]).before($('<div>').addClass('mobileatfmrec').addClass('noborder').append(window.mobileatfmrec));
+				$(headings[2]).before($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
 			}
 		}
 	});
