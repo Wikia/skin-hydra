2bbb21dfa02c478b462e5550a3c138dc762655f8
Alexia E. Smith <washuu@gmail.com>
2016-04-28 15:18:57 -0500

    Fix for Minerva.

--- js/mobileads.js
+++ js/mobileads.js
@@ -1,12 +1,10 @@
 (function(mw, $) {
 	mw.loader.using('skins.minerva.toggling').then(function() {
-		//if (window.mobileatfmrec && window.mobileatfmrec.length) {
+		if (window.mobileatfmrec && window.mobileatfmrec.length) {
 			var headings = $("#bodyContent").find("h1, h2, h3, h4, h5, h6");
-			if ($("#bodyContent").find(".content_block").length > 2) {
-				$('div#content_block_1').after($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
-			} else if ($(headings).length > 2) {
+			if ($(headings).length > 2) {
 				$(headings[2]).before($('<div>').addClass('mobileatfmrec').addClass('noborder').append(window.mobileatfmrec));
 			}
-		//}
+		}
 	});
 }(mediaWiki, jQuery));
