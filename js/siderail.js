7d7cb2c261d7faad73412797acae60ffdb69883a
Alexia E. Smith <washuu@gmail.com>
2016-05-10 11:18:30 -0500

    Handle DIV resize.

--- js/siderail.js
+++ js/siderail.js
@@ -1,9 +1,12 @@
 (function(mw, $) {
 	var sideRail = $('#siderail');
-	if ($(sideRail).length > 0 && $(sideRail).outerHeight() < $('#bodyContent').innerHeight()) {
+	if ($(sideRail).length > 0 && $(sideRail).outerHeight() < $('#bodyContent').outerHeight()) {
 		window.sideRailStartTop = $(sideRail).offset().top;
 		window.sideRailMarginTop = $(sideRail).css('margin-top');
 		window.maxsideRailMarginTop = $('#bodyContent').outerHeight() - $(sideRail).outerHeight();
+		$('#bodyContent').on('DOMSubtreeModified', function() {
+			window.maxsideRailMarginTop = $('#bodyContent').outerHeight() - $(sideRail).outerHeight();
+		});
 		$(window).scroll(function() {
 			var offset = 10;
 			if (Math.ceil(window.sideRailStartTop - offset) < Math.ceil($(document).scrollTop())) {
