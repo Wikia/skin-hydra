caaf044749a08e5d298217c18cf1e0db301551e8
Alexia E. Smith <washuu@gmail.com>
2016-05-19 14:56:31 -0500

    Eh

--- js/siderail.js
+++ js/siderail.js
@@ -1,5 +1,32 @@
 (function(mw, $) {
 	var sideRail = $('#siderail');
+	if ($(sideRail).length > 0 && $(sideRail).outerHeight() < $('#bodyContent').outerHeight()) {
+		window.sideRailStartTop = $(sideRail).offset().top;
+		window.sideRailMarginTop = $(sideRail).css('margin-top');
+		updateMaxSideRailMarginTop(sideRail);
+		$('#bodyContent').on('DOMSubtreeModified', function() {
+			updateMaxSideRailMarginTop(sideRail);
+		});
+		$(sideRail).on('DOMSubtreeModified', function() {
+			updateMaxSideRailMarginTop(sideRail);
+		});
+		$(window).resize(function() {
+			updateMaxSideRailMarginTop(sideRail);
+		});
+		$(window).scroll(function() {
+			var offset = 10;
+			if (Math.ceil(window.sideRailStartTop - offset) < Math.ceil($(document).scrollTop())) {
+				var calcMarginTop = $(document).scrollTop() - window.sideRailStartTop + offset;
+				$(sideRail).css('margin-top', (calcMarginTop < window.maxsideRailMarginTop ? calcMarginTop : window.maxsideRailMarginTop) + 'px');
+			} else if (window.sideRailStartTop >= $(document).scrollTop()) {
+				$(sideRail).css('margin-top', window.sideRailMarginTop);
+			}
+		});
+	}
+
+	function updateMaxSideRailMarginTop(sideRail) {
+		window.maxsideRailMarginTop = $('#bodyContent').outerHeight() - $(sideRail).outerHeight();
+	}
 
 	/* Hide siderail when we are editing with VE */
 	$("#ca-ve-edit span a").click(function(){
