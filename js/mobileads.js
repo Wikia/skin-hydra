7b2a385d74ac31c74d5c090f145d2221b0a24464
Alexia E. Smith <washuu@gmail.com>
2016-06-15 10:19:25 -0500

    Fix placement.

--- js/mobileads.js
+++ js/mobileads.js
@@ -1,7 +1,7 @@
 (function(mw, $) {
 	mw.loader.using('skins.minerva.toggling').then(function() {
 		if (window.mobileatfmrec && window.mobileatfmrec.length) {
-			var headings = $("#bodyContent").find("h1, h2, h3, h4, h5, h6");
+			var headings = $("#bodyContent").find("> h1, > h2, > h3, > h4, > h5, > h6");
 			if ($(headings).length > 2) {
 				$(headings[2]).before($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
 			}
