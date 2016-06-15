d22b30a93f00e4bec510bfdbd998489275d63f4f
Alexia E. Smith <washuu@gmail.com>
2016-06-15 14:40:52 -0500

    Heading helper.

--- js/mobileads.js
+++ js/mobileads.js
@@ -2,9 +2,13 @@
 	mw.loader.using('skins.minerva.toggling').then(function() {
 		if (window.mobileatfmrec && window.mobileatfmrec.length) {
 			var headings = $("#bodyContent").find("> h1, > h2, > h3, > h4, > h5, > h6");
+			var heading = false;
 			if ($(headings).length > 2) {
-				$(headings[2]).before($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
+				heading = headings[2];
+			} else if ($(headings).length > 0) {
+				heading = headings[0];
 			}
+			$(heading).before($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
 		}
 		$('body').append($("<div>").attr("id", "cdm-zone-end"));
 	});
