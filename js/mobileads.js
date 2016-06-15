c6ac42dac094817fd419a58dc0bfb772c4b46cec
Alexia E. Smith <washuu@gmail.com>
2016-06-15 15:07:23 -0500

    Logic fix.

--- js/mobileads.js
+++ js/mobileads.js
@@ -2,13 +2,13 @@
 	mw.loader.using('skins.minerva.toggling').then(function() {
 		if (window.mobileatfmrec && window.mobileatfmrec.length) {
 			var headings = $("#bodyContent").find("> h1, > h2, > h3, > h4, > h5, > h6");
-			var heading = false;
+			var adUnit = $('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec);
 			if ($(headings).length > 2) {
 				heading = headings[2];
+				$(headings[2]).before(adUnit);
 			} else if ($(headings).length > 0) {
-				heading = headings[0];
+				$("#bodyContent").prepend(adUnit);
 			}
-			$(heading).before($('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec));
 		}
 		$('body').append($("<div>").attr("id", "cdm-zone-end"));
 	});
