763902e2bed35a6d65b34504164922473ef92f83
Alexia E. Smith <washuu@gmail.com>
2016-06-16 18:03:23 -0500

    Fix this.

--- js/mobileads.js
+++ js/mobileads.js
@@ -7,7 +7,7 @@
 
 		var headings = $("#bodyContent").find("> h1, > h2, > h3, > h4, > h5, > h6");
 		if (window.mobileatfmrec && window.mobileatfmrec.length) {
-			var adUnit = $('<div>').addClass('mobileatfmrec').append(window.mobileatfmrec);
+			var adUnit = $('<div id="mobileatfmrec">').append(window.mobileatfmrec);
 			if ($(headings).length > 1) {
 				$(headings[1]).before(adUnit);
 			} else if ($(headings).length > 0) {
@@ -16,7 +16,7 @@
 		}
 		if (window.mobilebtfmrec && window.mobilebtfmrec.length) {
 			if ($(headings).length > 5) {
-				var btfMrecUnit = $('<div>').addClass('mobilebtfmrec').append(window.mobilebtfmrec);
+				var btfMrecUnit = $('<div id="mobilebtfmrec">').append(window.mobilebtfmrec);
 				$(headings[4]).before(btfMrecUnit);
 			}
 		}
