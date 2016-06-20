e92fdcf4a151e1b900df9b677a7322d786812f9a
Alexia E. Smith <washuu@gmail.com>
2016-06-20 13:28:33 -0500

    CPM Star

---
+++ js/cpmstar.js
@@ -0,0 +1,137 @@
+var cpmstar_editorials = function(j) {
+	var k = [],
+		l = function() {
+			this.cpmStarMultiAdInfo = [];
+			this.adCounter = 0
+		};
+	l.prototype = {
+		getCurrentAdInfo: function() {
+			return this.cpmStarMultiAdInfo[this.adCounter]
+		},
+		getAuthor: function() {
+			return "undefined" == typeof this.getCurrentAdInfo().author ? "" : this.getCurrentAdInfo().author
+		},
+		getImageUrl: function(a, c) {
+			for (var f = this.getCurrentAdInfo().imgs, m = a / c, g = a * c, n = {
+				url: "",
+				width: a,
+				height: c
+			}, h = -1, i = 0; i < f.length; i++) {
+				var b = f[i],
+					e, d = b;
+				e = 10 * (Math.min(d.width / d.height, m) / Math.max(d.width /
+					d.height, m));
+				d = Math.min(d.width * d.height, g) / Math.max(d.width * d.height, g);
+				e = e + d || 0;
+				e > h && (h = e, n = b)
+			}
+			return n.url
+		},
+		getLink: function() {
+			return this.getCurrentAdInfo().link
+		},
+		getTitle: function() {
+			return this.getCurrentAdInfo().title
+		},
+		getDescription: function(a) {
+			var a = "undefined" == typeof a ? 255 : a,
+				c = this.getCurrentAdInfo().desc;
+			"undefined" != typeof c && c.length > a && (c = c.substr(0, a - 3) + "...");
+			return c
+		},
+		nextAd: function() {
+			this.adCounter++
+		},
+		numAds: function() {
+			return this.cpmStarMultiAdInfo.length
+		},
+		adsLeft: function() {
+			return this.cpmStarMultiAdInfo.length -
+				this.adCounter
+		}
+	};
+	var s = function(a) {
+			a.callback = function(c) {
+				var g = new l,
+					f = a.cpmstar_callback;
+				if ("undefined" != typeof c.creatives)
+					for (var h = 0; h < c.creatives.length; h++) {
+						var i = g,
+							b = c.creatives[h],
+							e = [];
+						e.author = b.creativemacros.AUTHOR;
+						e.title = b.creativemacros.TITLE;
+						e.desc = b.creativemacros.DESC;
+						e.link = b.click;
+						var d = [];
+						"" != b.creativemacros.IMAGE60X90 && d.push({
+							url: b.creativemacros.IMAGE60X90,
+							width: 60,
+							height: 90
+						});
+						"" != b.creativemacros.IMAGE90X60 && d.push({
+							url: b.creativemacros.IMAGE90X60,
+							width: 90,
+							height: 60
+						});
+						"" !=
+						b.creativemacros.IMAGE90X90 && d.push({
+							url: b.creativemacros.IMAGE90X90,
+							width: 90,
+							height: 90
+						});
+						"" != b.creativemacros.IMAGE140X105 && d.push({
+							url: b.creativemacros.IMAGE140X105,
+							width: 140,
+							height: 105
+						});
+						"" != b.creativemacros.IMAGE140X140 && d.push({
+							url: b.creativemacros.IMAGE140X140,
+							width: 140,
+							height: 140
+						});
+						"" != b.creativemacros.IMAGE180X100 && d.push({
+							url: b.creativemacros.IMAGE180X100,
+							width: 180,
+							height: 100
+						});
+						"" != b.creativemacros.IMAGE180X250 && d.push({
+							url: b.creativemacros.IMAGE180X250,
+							width: 180,
+							height: 250
+						});
+						e.imgs = d;
+						i.cpmStarMultiAdInfo.push(e)
+					}
+				k.push(g);
+				a.adContainer ? p = setInterval(function() {
+					if (document.getElementById(a.adContainer)) {
+						clearInterval(p);
+						g.adContainer = a.adContainer;
+						var b = a.cpmstar_callback;
+						b(g)
+					}
+				}, 10) : f(g)
+			};
+			var c = Math.round(999999 * Math.random()),
+				f = document.createElement("script");
+			f.type = "text/javascript";
+			f.async = !0;
+			f.src = "undefined" != typeof a.cpmstar_subpoolid ? "//rigby.gamepedia.com/view.aspx?poolid=" + a.cpmstar_pid + "&subpoolid=" + a.cpmstar_subpoolid + "&multi=" + a.cpmstar_multi + "&json=nc_editorial&callback=this.cpmstar_dynamic_editorials." +
+				a.key + ".callback&rnd=" + c : "//rigby.gamepedia.com/view.aspx?poolid=" + a.cpmstar_pid + "&multi=" + a.cpmstar_multi + "&json=nc_editorial&callback=this.cpmstar_dynamic_editorials." + a.key + ".callback&rnd=" + c;
+			c = document.getElementsByTagName("script")[0];
+			c.parentNode.insertBefore(f, c)
+		},
+		p;
+	if ("undefined" != typeof j)
+		for (var q in j) {
+			var r = j[q];
+			r.key = q;
+			s(r)
+		}
+	return {
+		totalEditorials: function() {
+			return k.length
+		}
+	}
+}(this.cpmstar_dynamic_editorials);
