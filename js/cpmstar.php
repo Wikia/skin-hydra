26dde9de2c8eaed7dfb9379e404865a0fcb9af03
Alexia E. Smith <washuu@gmail.com>
2016-06-20 15:18:25 -0500

    Update

--- js/cpmstar.php
+++ js/cpmstar.php
@@ -1,8 +1,18 @@
 <?php
-$urlParts = explode($_SERVER['SCRIPT_FILENAME'].'?', $_SERVER['REQUEST_URI']);
+$script = str_replace(__DIR__.'/', '', __FILE__);
+$urlParts = explode($script.'?', $_SERVER['REQUEST_URI']);
 if (empty($urlParts[1])) {
 	http_response_code(404);
 	exit;
 }
-$url = str_replace(['/cdn2', '/cdn3'], ['cdn2.cmpstar.com', 'cdn3.cpmstar.com'], $urlParts[1]);
-var_dump($url);
+$url = str_replace(['/cdn2', '/cdn3'], ['http://cdn2.cmpstar.com', 'cdn3.cpmstar.com'], $urlParts[1]);
+if (filter_var($url, FILTER_VALIDATE_URL)) {
+	define("CA_COOKIE_SETUP", true);
+
+	putenv("MW_INSTALL_PATH=".dirname(dirname(__DIR__)));
+	require(dirname(dirname(__DIR__)).'/includes/WebStart.php');
+
+	$contents = Http::get($url);
+	var_dump($contents);
+}
+http_response_code(404);
