25049e2a34756deba5ed278d1d284f3a620cef8d
Alexia E. Smith <washuu@gmail.com>
2016-06-20 14:29:54 -0500

    Image helper.

--- js/cpmstar.php
+++ js/cpmstar.php
@@ -1,8 +1,8 @@
 <?php
-$urlParts = explode($_SERVER['SCRIPT_FILENAME'], $_SERVER['REQUEST_URI']);
+$urlParts = explode($_SERVER['SCRIPT_FILENAME'].'?', $_SERVER['REQUEST_URI']);
 if (empty($urlParts[1])) {
 	http_response_code(404);
 	exit;
 }
-$url = str_replace(['cd2', 'cdn3'], ['cdn2.cmpstar.com', 'cdn3.cpmstar.com'], $urlParts[1]);
+$url = str_replace(['/cdn2', '/cdn3'], ['cdn2.cmpstar.com', 'cdn3.cpmstar.com'], $urlParts[1]);
 var_dump($url);
