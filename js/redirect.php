3112994829649aaf9713404dd96f5c7c9fde7627
Alexia E. Smith <washuu@gmail.com>
2016-06-23 10:26:25 -0500

    URL redirect.

--- js/redirect.php
+++ js/redirect.php
@@ -5,20 +5,26 @@ if (empty($urlParts[1])) {
 	http_response_code(404);
 	exit;
 }
-$url = str_replace(['/cdn2', '/cdn3'], ['http://cdn2.cpmstar.com', 'http://cdn3.cpmstar.com'], $urlParts[1]);
+$url = str_replace(['server', '/cdn2', '/cdn3'], ['http://server.cpmstar.com', 'http://cdn2.cpmstar.com', 'http://cdn3.cpmstar.com'], $urlParts[1]);
 if (filter_var($url, FILTER_VALIDATE_URL)) {
-	define("CA_COOKIE_SETUP", true);
-
-	$IP = dirname(dirname(dirname(__DIR__)));
-	putenv("MW_INSTALL_PATH=".$IP);
-	require($IP.'/includes/WebStart.php');
-
-	$contents = Http::get($url);
-	if ($contents !== false) {
-		$extension = substr($url, strrpos($url, '.') + 1);
-		header('Content-Type: image/'.$extension);
-		echo $contents;
+	if (strpos('http://server.cpmstar.com', $url) !== false) {
+		header('Location: '.$url);
+		http_response_code(302);
 		exit;
+	} else {
+		define("CA_COOKIE_SETUP", true);
+
+		$IP = dirname(dirname(dirname(__DIR__)));
+		putenv("MW_INSTALL_PATH=".$IP);
+		require($IP.'/includes/WebStart.php');
+
+		$contents = Http::get($url);
+		if ($contents !== false) {
+			$extension = substr($url, strrpos($url, '.') + 1);
+			header('Content-Type: image/'.$extension);
+			echo $contents;
+			exit;
+		}
 	}
 }
 http_response_code(404);
