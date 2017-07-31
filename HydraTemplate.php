6d9c81a98e9697f869b511e265df81d634e4d96c
Alexia E. Smith <washuu@gmail.com>
2017-07-31 10:37:14 -0500

    Syntax.

--- HydraTemplate.php
+++ HydraTemplate.php
@@ -352,13 +352,13 @@ class HydraTemplate extends VectorTemplate {
 					}
 					?>" aria-labelledby="p-sharing-label">
 						<h3 id="p-sharing-label"><span><?php
-							if (isset($this->data['sharing_urls']['share']['text']) {
+							if (isset($this->data['sharing_urls']['share']['text'])) {
 								echo $this->data['sharing_urls']['share']['text'];
 							}
 						?></span><a href="#"></a></h3>
 						<div class="menu">
 							<?php
-								if (isset($this->data['sharing_urls']['share']['html']) {
+								if (isset($this->data['sharing_urls']['share']['html'])) {
 									echo $this->data['sharing_urls']['share']['html'];
 								}
 							?>
